8f823b85b0222ec25898e1ee1ffd6bb430ee017a
Michael Sweeney <michaelsweeney@Sweeney-MacBook-Air.local>
2021-04-16 14:54:59 -0400

    first working draft

--- pyll97.py
+++ pyll97.py
@@ -1,9 +1,8 @@
-import os
-import re
-import pandas as pd
 
-CO2_LIMITS_BY_BUILDING_TYPE = {
-    'A':	[0.01074, 0.0042, 0.0014],
+### CONSTANTS ###
+
+co2_limits = {
+    'A': [0.01074, 0.0042, 0.0014],
     'B_HEALTH':	[0.02381, 0.0133, 0.0014],
     'B_REGULAR': [0.00846, 0.00453, 0.0014],
     'E': [0.00758,	0.00344, 0.0014	],
@@ -20,26 +19,103 @@ CO2_LIMITS_BY_BUILDING_TYPE = {
     'U': [0.00426, 0.0011, 0.0014],
 }
 
-CARBON_CONVERSION = {
-    'elec': 0.000288962,  # tCO2 / kWh
-    'gas': 0.005311,  # tCO2 / Therm
-    'steam': 0.053646,  # tCO2/MMBtu (from 66.4 kg/MMBtu)
-    'fuel_two': '???',
-    'fuel_four': '???'
+# kbtu to co2
+carbon_conversion_rates = {
+    'elec': 0.000084689,
+    'gas': 0.00005311,
+    'steam': 0.00004493,
+    'fuel_two': 0.00007421,
+    'fuel_four': 0.00007529
 }
 
-KBTU_CONVERSION = {  # convert to kbtu
+# convert kwh / therm / lbs / gal to kbtu
+native_to_kbtu_conversion = {
     'elec': 3.412,
     'gas': 100,
     'steam': 1194,
-    'fuel_two': '???',
-    'fuel_four': '???'
+    'fuel_two': 138,
+    'fuel_four': 146
 }
-'d'
+
+fine_per_ton_co2 = 268
+
+
+
+def get_total_area(bldgtypes):
+    area = 0
+    for t in bldgtypes:
+        area += t['area']
+    return area
+
+
+def get_carbon_limits(bldgtypes):
+    limit_2024 = 0
+    limit_2030 = 0
+    limit_2034 = 0
+
+    for t in bldgtypes:
+        tag = t['type']
+        area = t['area']
+        limits = co2_limits[tag]
+
+        limit_2024 += area * limits[0]
+        limit_2030 += area * limits[1]
+        limit_2034 += area * limits[2]
+
+    return {
+        '2024': limit_2024,
+        '2030': limit_2030,
+        '2034': limit_2034
+    }
+
+
+def get_total_carbon(consumptiondict):
+    carbon = 0
+    for key, val in consumptiondict.items():
+        consumption_kbtu = native_to_kbtu_conversion[key] * val
+        carbon += carbon_conversion_rates[key] * consumption_kbtu
+    return carbon
+
+
+def get_fines(limits, total_carbon):
+    fines = {}
+    for year, limit in limits.items():
+        if total_carbon < limit:
+            fines[year] = 0
+        else:
+            fine = fine_per_ton_co2 * (total_carbon - limit)
+            fines[year] = fine
+    return fines
+
 
 def get_ll97_summary(bldgtypes, utilityconsumption):
-    print(bldgtypes)
-    return
+    '''
+    main function. 
+    
+    arguments:
+    - bldgtypes: list of dictionaries with 'type' and 'area' keys
+    - utilityconsumption: dictionary with 'elec', 'gas', 'steam', 'fuel_two', 'fuel_four' keys
 
+    returns: summary dictionary with fines, limits and total carbon.
 
-a = '2'
+    '''
+    limits = get_carbon_limits(bldgtypes)
+    total_carbon = get_total_carbon(utilityconsumption)
+    fines = get_fines(limits, total_carbon)
+
+    if get_total_area(bldgtypes) < 20000:
+        summary = {
+            'limits': {'2024': 0, '2030': 0, '2034': 0},
+            'total_carbon': total_carbon,
+            'fines': {'2024': 0, '2030': 0, '2034': 0}
+        }
+
+    else:
+        summary = {
+            'limits': limits,
+            'total_carbon': total_carbon,
+            'fines': fines
+        }
+
+    return summary
+
